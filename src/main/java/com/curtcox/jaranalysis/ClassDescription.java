7f8a1a52af3f70fba357b008dfb07d15d9e5b940
Curt Cox <curtcox@gmail.com>
2022-07-11 15:52:44 -0500

    Added ClassDescription.

---
+++ src/main/java/com/curtcox/jaranalysis/ClassDescription.java
@@ -0,0 +1,50 @@
+package com.curtcox.jaranalysis;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+final class ClassDescription {
+    final Class c;
+    final ClassNameType root;
+    private final Set<ClassNameType> types = new HashSet<>();
+    final Set<Uses> uses = new HashSet<>();
+
+    int partition;
+
+    ClassDescription(Class fullName, ClassNameType root) {
+        this.c = fullName;
+        this.root = root;
+        if (root!=null) {
+            types.add(root);
+        }
+    }
+
+    void markAsUsedBy(ClassNameType type) {
+        types.add(type);
+    }
+    static String csvHeader() {
+        return csv("jar","root", "partition", csv(ClassNameType.values()),"fullName","packageName","shortName", csv(Uses.values()));
+    }
+
+    public String toCSV() {
+        if (types.isEmpty()) {
+            System.out.println(c + " has no types.");
+        }
+        return csv(jar(),root(),partition,csv(ClassNameType.values(),types),c,c.packageName(),c.shortName(),csv(Uses.values(),uses));
+    }
+
+    String jar() { return c.jar == null ? "" : c.jar; }
+    String root() { return root == null ? "" : root.name; }
+
+    @Override public String toString() { return c + " " + root + " " + types + " " + uses; }
+
+    static String csv(Object... values) { return csv(values, Object::toString); }
+
+    static String csv(Object[] values,EnumSet<?> set) { return csv(values, (v) -> set.contains(v) ? v.toString() : ""); }
+
+    static String csv(Object[] values, Function<Object,String> mapper) {
+        return Arrays.stream(values).map(mapper).collect(Collectors.joining(","));
+    }
+
+}
--- src/main/java/com/curtcox/jaranalysis/Uses.java
+++ src/main/java/com/curtcox/jaranalysis/Uses.java
@@ -1,7 +1,14 @@
 package com.curtcox.jaranalysis;
 
+import java.util.*;
+
 final class Uses {
     final String text;
+
+    private static List<ClassNameType> all = new ArrayList<>();
+
+    static List<ClassNameType> values() { return all; }
+
     Uses(String text) {
         this.text = text;
     }
